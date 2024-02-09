e75a3e3241203f4c014d25174a1dabf3919bba46
taniya-ignatius <itaniya895@gmail.com>
2024-02-09 11:27:19 +0530

    password secured

--- controllers/UserRouter.js
+++ controllers/UserRouter.js
@@ -1,15 +1,34 @@
-const express=require("express")
-const router=express.Router()
+const express = require("express")
+const router = express.Router()
+const bcrypt = require("bcryptjs")
 
-const userModel=require("../model/UserModel")
-router.post("/signup",async(req,res)=>{
-    let data=req.body
-    let blog=new userModel(data)
-    let result=await blog.save()
-    res.json({
-        status:"success"
-    })
+hashPasswordGenerator = async (pass) => {
+    const salt = await bcrypt.genSalt(10)
+    return bcrypt.hash(pass, salt)
+}
+
+const userModel = require("../model/UserModel")
+router.post("/signup", async (req, res) => {
+
+    let { data } = { "data": req.body }
+    let password = data.password
+    hashPasswordGenerator(password).then(
+        (hashedPassword) => {
+            console.log(hashedPassword)
+            data.password = hashedPassword
+            console.log(data)
+            let blog = new userModel(data)
+            let result = blog.save()
+            res.json({
+                status: "success"
+            })
+        }
+    )
+
+
+
+
 })
 
 
-module.exports=router
+module.exports = router
